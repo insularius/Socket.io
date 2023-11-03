48109c46126217aa849674ace47ebeb7c1a4a40a
Sultanbek <sultan.sekenov@yandex.ru>
2023-11-03 09:57:06 +0600

    1

--- chat_app/server.js
+++ chat_app/server.js
@@ -1,7 +1,14 @@
 const http = require("http");
 const { Server } = require("socket.io");
 const httpServer = http.createServer();
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["http://localhost:3000", "http://local.daru.kz:3000"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+});
 
 io.on("connection", (socket) => {
   console.log("A user connected:", socket.id);
